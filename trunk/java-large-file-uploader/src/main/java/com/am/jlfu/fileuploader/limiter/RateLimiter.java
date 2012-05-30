78d3537fbc4e29e146b340e62e11a8ed7a600cf1
meyer.ant@gmail.com <meyer.ant@gmail.com@c4ad9de1-db66-7cbc-96bc-fbcec12455a6>
2012-05-30 13:18:37 +0000

    commit for history

---
+++ trunk/java-large-file-uploader/src/main/java/com/am/jlfu/fileuploader/limiter/RateLimiter.java
@@ -0,0 +1,2 @@
+moved to:
+http://code.google.com/p/java-large-file-uploader/source/browse/trunk/java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/fileuploader/limiter/RateLimiter.java
