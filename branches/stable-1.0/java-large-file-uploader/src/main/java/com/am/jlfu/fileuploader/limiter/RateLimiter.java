302a1e922246f6b70fb18cc40733f3a3bab420b7
meyer.ant@gmail.com <meyer.ant@gmail.com@c4ad9de1-db66-7cbc-96bc-fbcec12455a6>
2012-06-01 14:06:11 +0000

    delete branches

--- branches/stable-1.0/java-large-file-uploader/src/main/java/com/am/jlfu/fileuploader/limiter/RateLimiter.java
+++
@@ -1,2 +0,0 @@
-moved to:
-http://code.google.com/p/java-large-file-uploader/source/browse/trunk/java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/fileuploader/limiter/RateLimiter.java
