5d5aa087d580c82021e8700539586a18bcfb9527
meyer.ant <meyer.ant@gmail.com>
2012-06-05 09:44:19 +0000

    test correction

--- java-large-file-uploader-parent/java-large-file-uploader-jar/src/test/java/com/am/jlfu/fileuploader/limiter/UploadProcessingOperationManagerTest.java
+++ java-large-file-uploader-parent/java-large-file-uploader-jar/src/test/java/com/am/jlfu/fileuploader/limiter/UploadProcessingOperationManagerTest.java
@@ -25,7 +25,8 @@ public class UploadProcessingOperationManagerTest {
 
 	@Before
 	public void before() {
-
+		uploadProcessingOperationManager.clientsAndRequestsProcessingOperation.clear();
+		uploadProcessingOperationManager.clientToRequestsMapping.clear();
 	}
 
 
