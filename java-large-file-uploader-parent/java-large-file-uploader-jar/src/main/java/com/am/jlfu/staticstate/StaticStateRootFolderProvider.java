89756a40c7b6e67c8e0f4ec647c06a311b0eb5b6
meyer.ant <meyer.ant@gmail.com>
2012-06-21 13:29:36 +0000

    do not use a provider anymore but uses a property.

--- java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/staticstate/StaticStateRootFolderProvider.java
+++ java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/staticstate/StaticStateRootFolderProvider.java
@@ -3,16 +3,43 @@ package com.am.jlfu.staticstate;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
 
 
 /**
  * Provides the root folder in which files will be uploaded.<br>
- * This interface has to be implemented by a spring bean.
  * 
  * @author antoinem
  * 
  */
-public interface StaticStateRootFolderProvider {
-
-	File getRootFolder();
+@Component
+public class StaticStateRootFolderProvider {
+	
+	@Value("${jlfu.defaultUploadFolder:/JavaLargeFileUploader}")
+	private String defaultUploadFolder;
+	
+	@Autowired(required = false)
+	WebApplicationContext webApplicationContext;
+	
+	public File getRootFolder() {
+		String realPath = webApplicationContext.getServletContext().getRealPath(defaultUploadFolder);
+		File file = new File(realPath);
+		// create if non existent
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+		// if existent but a file, runtime exception
+		else {
+			if (file.isFile()) {
+				throw new RuntimeException(file.getAbsolutePath() +
+						" is a file. The default root folder provider uses this path to store the files. Consider using a specific root folder provider or delete this file.");
+			}
+		}
+		return file;
+	}
+	
 }
