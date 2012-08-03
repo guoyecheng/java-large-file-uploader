6672ebdfc8c255e7d68f80639eea29a60cfb84a6
meyer.ant <meyer.ant@gmail.com>
2012-08-03 09:50:53 +0000

    improved logging (Arrays.toString())

--- java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/fileuploader/exception/AuthorizationException.java
+++ java-large-file-uploader-parent/java-large-file-uploader-jar/src/main/java/com/am/jlfu/fileuploader/exception/AuthorizationException.java
@@ -1,6 +1,7 @@
 package com.am.jlfu.fileuploader.exception;
 
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import com.am.jlfu.fileuploader.web.UploadServletAction;
@@ -10,7 +11,7 @@ import com.am.jlfu.fileuploader.web.UploadServletAction;
 public class AuthorizationException extends Exception {
 
 	public AuthorizationException(UploadServletAction actionByParameterName, UUID clientId, UUID... optionalFileIds) {
-		super("User " + clientId + " is not authorized to perform " + actionByParameterName + (optionalFileIds != null ? " on " + optionalFileIds : ""));
+		super("User " + clientId + " is not authorized to perform " + actionByParameterName + (optionalFileIds != null ? " on " + Arrays.toString(optionalFileIds) : ""));
 	}
 
 
